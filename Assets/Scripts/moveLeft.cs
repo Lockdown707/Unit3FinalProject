88856bc8f8d5e7fbccd2eca25898f89ec16ed621
Lockdown707 <145475291+Lockdown707@users.noreply.github.com>
2023-10-19 10:14:56 -0700

    Added un Modified Particle System
    Need to adjust Particle System for death explosion and foot jets.
    
    Also, Completed move left script.

--- Assets/Scripts/moveLeft.cs
+++ Assets/Scripts/moveLeft.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class moveLeft : MonoBehaviour
 {
+    public float speed = 30f;
+    private playerController playerControllerScript;
+    private float backBound = -20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerControllerScript.isDead == false)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (transform.position.z < backBound && gameObject.CompareTag("obstaclePrefab"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
