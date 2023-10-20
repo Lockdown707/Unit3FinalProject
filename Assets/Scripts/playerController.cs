dfc4aedcfc8391de5381fa38e3ece2280b4b78d3
Lockdown707 <145475291+Lockdown707@users.noreply.github.com>
2023-10-20 10:11:51 -0700

    Completed unit 3 Final Project
    next Step add Tie Fighters to the list of obstacles and instantiate from an array.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,11 +8,19 @@ public class playerController : MonoBehaviour
     private Animator animator;
     public bool isDead;
     public float jumpForce;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem jetParticle;
+    public AudioClip deadSound;
+    public AudioClip flapSound;
+    private AudioSource playerAudio;
+    public AudioSource backgroundAudio;
+    public Rigidbody prefabRb;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();   
     }
 
         // Update is called once per frame
@@ -22,12 +30,20 @@ public class playerController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.Space) && !isDead)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAudio.PlayOneShot(flapSound);
         }  
     }
     private void OnCollisionEnter(Collision collision)
     {
         isDead = true;
         animator.SetTrigger("Die");
+        explosionParticle.Play();
+        playerAudio.PlayOneShot(deadSound);
+        backgroundAudio.Stop();
+        jetParticle.Stop();
+        prefabRb = collision.gameObject.GetComponent<Rigidbody>();
+        prefabRb.isKinematic = true;
+
     }
 }
